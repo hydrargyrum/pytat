fdca54db515de76d631071d409f4fd32391ae3b0
hydrargyrum <dev@indigo.re>
2018-08-24 22:30:24 +0200

    fix output over-indentation because of print separator

--- pytat/lib.py
+++ pytat/lib.py
@@ -47,7 +47,7 @@ class ReplacerVisitor(ast.NodeTransformer):
 
             if self.separators:
                 print('#=# generated code')
-            print((node.col_offset - 1) * ' ', astor.to_source(ret))
+            print(node.col_offset * ' ', astor.to_source(ret), sep='')
             if self.separators:
                 print('#=# end generated code')
 
